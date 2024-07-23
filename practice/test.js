fdb0be6ec49ff60b8594016cb0189b30576c1bc5
Vimal-2005 <vineshpagarani@gmail.com>
2024-07-23 16:01:54 +0000

    till string

--- practice/test.js
+++ practice/test.js
@@ -1,1 +1,13 @@
 console.log("Hey Vimal")
+
+// let name = "vimal"  // datatypes can use let as a int, stringm boolean
+// const val = 2222 // can take constant values
+
+// console.log('Hey my name is ${name} , and I am working as wordpress manager')
+//we use this when we use string int in console
+
+//const name = new String('Vimal')   ..  another method to create string as a object
+
+// const url = "https://vimal.com/vimal%20.vimal"
+// to replace anything from string we use
+// console.log(url.replace('%20','-'))

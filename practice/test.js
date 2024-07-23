a7ecf6578231435c28766fa6029131220ec4786e
Vimal-2005 <vineshpagarani@gmail.com>
2024-07-23 16:58:41 +0000

    date and time

--- practice/test.js
+++ practice/test.js
@@ -11,3 +11,9 @@ console.log("Hey Vimal")
 // const url = "https://vimal.com/vimal%20.vimal"
 // to replace anything from string we use
 // console.log(url.replace('%20','-'))
+
+// console.log(Math.round(Math.random() * (100000 - 100) + 100))
+
+let newDate = new Date()
+// console.log(newDate) -- Output  2024-07-23T16:44:25.817Z
+//console.log(newDate.toString()) -- Output Tue Jul 23 2024 16:45:40 GMT+0000 (Coordinated Universal Time)
